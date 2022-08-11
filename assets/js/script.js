9f32ee8c5d660bda1b5a2a4976b0ea9a5820e75f
fersatir <fatihersatir@gmail.com>
2022-08-11 12:48:45 +0300

    aritmetics-operators

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,33 @@
-console.log("Bu bir external scriptir");
+
+
+
+
+
+
+let isim = "merhaba";
+
+console.log(isim);
+
+console.log(isim.charAt(0));
+
+console.log(isim.substring(1));
+
+console.log(typeof(isim));
+
+let a = "5";
+
+let b = 6;
+
+console.log(typeof(a));
+
+console.log(a+b);
+
+console.log(Number(a)+b);
+
+let sayi = "5.5";
+
+console.log(parseFloat(sayi) + b);
+
 
 
 
